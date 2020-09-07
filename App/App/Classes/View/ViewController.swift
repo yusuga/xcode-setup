d72699de3f6c6d98dcde2d4422c3c52acd03ce1e
yusuga <dev@yusuga.com>
2020-09-07 22:42:40 +0900

    Add xcodeproj

---
+++ App/App/Classes/View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by yusuga on 2020/09/07.
+//  Copyright © 2020 yusuga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ App/AppTests/AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  AppTests.swift
+//  AppTests
+//
+//  Created by yusuga on 2020/09/07.
+//  Copyright © 2020 yusuga. All rights reserved.
+//
+
+import XCTest
+@testable import App
+
+class AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
